435a9a2c2dba9a361cb2fd1da6db2ec0e11e2c17
Gabriel Pelincel Rmalho <gabrielpelincel@gmail.com>
2022-12-20 13:13:02 -0300

    Upload files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CommonCardComponent } from './common-card/common-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CommonCardComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/common-card/common-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-common-card',
+  templateUrl: './common-card.component.html',
+  styleUrls: ['./common-card.component.scss']
+})
+export class CommonCardComponent {
+
+}
